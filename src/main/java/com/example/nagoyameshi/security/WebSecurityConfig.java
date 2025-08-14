0e424c94446719ee96895814421dc58df28d102d
atakuya-12915 <t.taku0402@outlook.com>
2025-08-14 12:39:36 +0900

    add List of stores

--- src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
+++ src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
@@ -17,8 +17,8 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**")
-						.permitAll() 									// すべてのユーザーにアクセスを許可するURL
+						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**").permitAll() 	// すべてのユーザーにアクセスを許可するURL
+						.requestMatchers("/restaurants/**").hasAnyRole("ANONYMOUS", "FREE_MEMBER", "PAID_MEMBER")  // 未ログインのユーザー、無料会員、有料会員にアクセスを許可するURL
 						.requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
 						.anyRequest().authenticated() 					// 上記以外のURLはログインが必要（どのロールでもOK）
 				)
